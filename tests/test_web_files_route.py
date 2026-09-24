74b90a23ce2e40199446bfe61b0d2ab9edb399a6
ddse <ducdungse@gmail.com>
2026-09-24 17:33:49 +0900

    test: align track action route assertions with query params

--- tests/test_web_files_route.py
+++ tests/test_web_files_route.py
@@ -4,10 +4,20 @@ from web.app import app
 
 
 class FileRouteTests(unittest.TestCase):
-    def test_file_route_uses_query_track_id(self):
+    def test_file_download_route_uses_query_track_id(self):
         routes = [
             route for route in app.routes
             if getattr(route, "path", "") == "/api/files"
+            and "GET" in (getattr(route, "methods", set()) or set())
+            and any(param.name == "track_id" for param in route.dependant.query_params)
+        ]
+        self.assertEqual(len(routes), 1)
+
+    def test_file_delete_route_uses_query_track_id(self):
+        routes = [
+            route for route in app.routes
+            if getattr(route, "path", "") == "/api/files"
+            and "DELETE" in (getattr(route, "methods", set()) or set())
             and any(param.name == "track_id" for param in route.dependant.query_params)
         ]
         self.assertEqual(len(routes), 1)
